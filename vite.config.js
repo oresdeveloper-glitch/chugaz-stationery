1176f2eb9989fdcbbac8a79936a2068b5d92f7cd
orresydeveloper <orresydeveloper@users.noreply.github.com>
2026-09-05 15:31:45 +0300

    Fix vite.config.js and vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,6 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default { plugins: [react()], build: { outDir: 'frontend/dist' }, server: { port: 5173 } };
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({
+  plugins: [react()],
+  build: { outDir: 'frontend/dist' }
+})
