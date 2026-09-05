6afc79f9aee97169c830232c005b9d0c8ed39319
orresydeveloper <orresydeveloper@users.noreply.github.com>
2026-09-05 15:36:23 +0300

    Fix root dependencies and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
- plugins: [react()],
- server: {
-  port: 5173,
-  proxy: {
-   '/api': { target: 'http://localhost:4000', changeOrigin: true },
-   '/uploads': { target: 'http://localhost:4000', changeOrigin: true },
-  },
- },
- build: { outDir: 'dist' },
-});
+  plugins: [react()],
+  build: { outDir: 'frontend/dist' }
+})
