341f39a96b04d09112bb54b83d7ec27d09c61df0
orresydeveloper <orresydeveloper@users.noreply.github.com>
2026-09-09 04:29:29 +0300

    Fix require path in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
 const { NodeRuntime } = require('@vercel/node');
-const backendApp = require('./backend/server');
+const backendApp = require('../backend/server');
 module.exports = new NodeRuntime(backendApp);
